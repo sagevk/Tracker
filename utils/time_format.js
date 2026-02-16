2529fafa27b9887388b864bd82190a8ebc6bad93
saggy <165743781+sagevk@users.noreply.github.com>
2026-02-15 19:41:21 -0500

    A

---
+++ utils/time_format.js
@@ -0,0 +1,14 @@
+function format_timestamp(date = new Date()) {
+    return date.toLocaleString("en-US", {
+        timeZone: "America/New_York",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true,
+    });
+}
+
+module.exports = { format_timestamp };
